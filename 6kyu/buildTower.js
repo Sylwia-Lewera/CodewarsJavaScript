09a93b38e0a91e3713afcc5dc67f7279f28f2e3c
Sylwia Lewera <lewera.sylwia@gmail.com>
2026-04-16 18:50:41 +0200

    16-04-2026

---
+++ 6kyu/buildTower.js
@@ -0,0 +1,40 @@
+/**
+ Build Tower
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+]
+Go challenge Build Tower Advanced once you have finished this :)
+ */
+
+function towerBuilder(nFloors) {
+  let pyramid = [];
+
+  for (let i = 0; i < nFloors; i++) {
+    let spaces = nFloors - i - 1;
+    let stars = 2 * i + 1;
+
+    pyramid.push(
+      ' '.repeat(spaces) +
+      '*'.repeat(stars) +
+      ' '.repeat(spaces)
+    );
+  }
+
+  return pyramid;
+}
